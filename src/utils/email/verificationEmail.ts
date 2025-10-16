b65d26874d0c057f64d60b95e50e3e15701084db
Its-utsav <utsavdhimmar987@gmail.com>
2025-10-16 18:47:49 +0530

    feat: utils for password hashing, validation and email

---
+++ src/utils/email/verificationEmail.ts
@@ -0,0 +1,32 @@
+import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../../emails/verification-email";
+import { resend } from "../resend";
+
+export async function sendVerificationEmail({
+  email,
+  username,
+  otp,
+}: {
+  email: string;
+  username: string;
+  otp: string;
+}): Promise<ApiResponse> {
+  try {
+    const sendEmailResponse = resend.emails.send({
+      from: "True Feedback <onboarding@resend.dev>",
+      to: email,
+      subject: "Verification code for true feedback",
+      react: VerificationEmail({ username, otp }),
+    });
+    return {
+      message: "Email sent successfully",
+      success: false,
+    };
+  } catch (error) {
+    console.error(`[Error] verification email could'nt sent`, error);
+    return {
+      message: "Unable to send verification email",
+      success: false,
+    };
+  }
+}
---
+++ src/utils/validations.ts
@@ -0,0 +1,21 @@
+import { ZodObject, ZodRawShape } from "zod";
+
+export function parserInputWithZodSchema<T extends ZodRawShape>(
+  data: unknown,
+  schema: ZodObject<T>,
+) {
+  const zodResult = schema.safeParse(data);
+  if (zodResult.success) {
+    return {
+      success: true,
+      data: zodResult.data as T,
+      // message: "Validation success",
+    };
+  } else {
+    return {
+      success: false,
+      message: zodResult.error?.issues.map(({ message }) => message).join(", "),
+      // data: {},
+    };
+  }
+}
