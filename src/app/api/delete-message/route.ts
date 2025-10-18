bd540176c4e0e556d3944f8f1f0520c43087feab
utsav-dhimmar <utsavdhimmar987@gmail.com>
2025-10-18 19:52:36 +0530

    feat: delete message

---
+++ src/app/api/delete-message/route.ts
@@ -0,0 +1,66 @@
+import connectToDB from "@/db/dbConnect";
+import Messages from "@/model/messages.model";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(
+	request: NextRequest,
+	{ params }: { params: { messageId: string } },
+) {
+	try {
+		await connectToDB();
+		const { messageId } = params;
+		const session = await getServerSession();
+		if (!session || !session?.user) {
+			return NextResponse.json(
+				{
+					success: false,
+					message: "Not authenticated",
+				},
+				{ status: 401 },
+			);
+		}
+		const user = session.user;
+		// const userId = new mongoose.Schema.Types.ObjectId(user._id!);
+		const message = await Messages.findById(messageId);
+		if (message?.userId !== user._id) {
+			return NextResponse.json(
+				{
+					message: "This messages is not belongs to you :)",
+					success: false,
+				},
+				{ status: 401 },
+			);
+		}
+		const messageDeleteResponse =
+			await Messages.findByIdAndDelete(messageId);
+
+		if (!messageDeleteResponse) {
+			return NextResponse.json(
+				{
+					message: "Unable to delete message",
+					success: false,
+				},
+				{ status: 500 },
+			);
+		}
+		return NextResponse.json(
+			{
+				message: "message deleted successfully",
+				success: true,
+			},
+			{ status: 200 },
+		);
+	} catch (error) {
+		console.error("unable to delete message", error);
+		return NextResponse.json(
+			{
+				message: "unable to delete message",
+				success: false,
+			},
+			{
+				status: 500,
+			},
+		);
+	}
+}
