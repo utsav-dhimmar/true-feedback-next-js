1d16885c17aba6f3530f76bb31dbd751dac40549
utsav-dhimmar <utsavdhimmar987@gmail.com>
2025-11-02 13:46:37 +0530

    fix: redirect fix attempt 2

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,24 +1,21 @@
 import { getToken } from "next-auth/jwt";
-import { type NextRequest, NextResponse } from "next/server";
-// export { default } from "next-auth/middleware";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function proxy(request: NextRequest) {
-	const token = await getToken({
-		req: request,
-	});
-
-	const currentPath = request.nextUrl.pathname;
+export async function porxy(request: NextRequest) {
+	const token = await getToken({ req: request });
+	const { pathname } = request.nextUrl;
 
 	const publicPaths = ["/sign-in", "/sign-up", "/verify"];
 	const privatePaths = ["/dashboard"];
 
-	if (token && publicPaths.includes(currentPath)) {
+	if (token && publicPaths.includes(pathname)) {
 		return NextResponse.redirect(
 			new URL("/dashboard", request.nextUrl.origin),
 		);
 	}
 
-	if (!token && privatePaths.includes(currentPath)) {
+	if (!token && privatePaths.includes(pathname)) {
 		return NextResponse.redirect(
 			new URL("/sign-in", request.nextUrl.origin),
 		);
